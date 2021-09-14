3f2a267b46d7724b231a630deb51f3a0e745cbe2
Kavish-Coder <87529772+Kavish-Coder@users.noreply.github.com>
2021-09-14 14:30:04 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+function bed_room() {
+window.location="bed_room.html";
+}
+
+function tv_ac() {
+window.location="tv_ac.html";
+}
+
+function desk() {
+window.location="desk.html";
+}
+
+function bottles() {
+window.location="bottles.html";
+}
+
+function fruit_basket() {
+window.location="fruit_basket.html";
+}
+
+        
